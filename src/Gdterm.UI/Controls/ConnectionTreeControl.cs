ecef1c9ab253fc54e5fdadfbfa5017ff5d54e261
zgc <zgc@l450.local>
2026-09-05 17:12:24 +0800

    fix: restore cue-banner call on connection tree filter box

--- src/Gdterm.UI/Controls/ConnectionTreeControl.cs
+++ src/Gdterm.UI/Controls/ConnectionTreeControl.cs
@@ -62,7 +62,7 @@ namespace Gdterm.UI.Controls
                 Font = ResolveDefaultFont(),
                 Height = 24
             };
-            try { Gdterm.UI.Diagnostics._filterBox.PlaceholderText = "输入主机/名称/分组过滤…"; }
+            try { WinFormsCompat.SetCueBanner(_filterBox, "输入主机/名称/分组过滤…"); }
             catch { }
             _filterBox.TextChanged += (s, e) => ApplyFilter(_filterBox.Text);
 
